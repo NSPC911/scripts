a3f0a7390226c3a3f7addaf232f58e1939c83379
NSPC911 <notsoprocoder911@gmail.com>
2024-08-13 15:50:50 +0800

    Updated git+ script

--- git+.bat
+++ git+.bat
@@ -1,14 +1,25 @@
 @echo off
-if "%1"=="" (
-    echo Usage: git+.bat [squash/merge]
-    echo squash: Rebases onto main with interactive rebase
-    echo merge: Merges onto main
-    exit /b 1
-)
-if "%1"=="squash" (
-    git rebase -i main
-)
-if "%1"=="merge" AND NOT "%2"=="" (
-    git checkout main
-    git merge "%2"
+if "%1" == "squash" (
+    if "%2" == "abort" (
+        git rebase --abort
+    ) else (
+        git rebase -i main
+    )
+) else if "%1" == "merge" (
+    if "%2" == "" (
+        echo No branch is specified to merge.
+    ) else (
+        git checkout main
+        git merge %2
+    )
+) else if "%1" == "switch" (
+    if "%2" == "" (
+        echo No branch is specified to switch to.
+    ) else (
+        git branch --set-upstream-to=origin/%2 %2
+        git switch %2
+        git pull %2
+    )
+) else (
+    echo "git+.bat [squash/merge/switch] [abort/<branch>]"
 )
